46ad2f1d614c7a4236cbb484fdacc27674574f02
namila007 <namila007@gmail.com>
2016-04-02 23:26:38 +0530

    Calculator 2.0
    Calculator 2.0 w/ functions

---
+++ Calculator/Calculator 2.py
@@ -0,0 +1,70 @@
+import sys
+import ctypes
+first=None
+second=None
+op=None
+box=ctypes.windll.user32.MessageBoxW
+
+def start():
+
+	global first  #assigning global to local 
+	global second
+	global op
+	first=input(" |Enter First Number:")
+	op=input(" |Math functions available \n |1. '+'  addition       | \n |2.'-'  subtraction     | \n |3. '*'  multiplication | \n |4. '/'  division       | \n |Insert Math function number:")
+	#if op=="None":
+	#	box(None,"Enter a fucntion","Error",0)
+	#	op=input(" |Insert Math function number:")
+	#else:	
+	second=input(" |Enter Second Number:")
+	
+	
+	return (first,second,op) 
+
+def cal(fi,se):
+	ans=None
+	#ccalculation
+	
+	# _,_,op=start() old way to assign var for return fucntion
+	
+	if op == "1":
+		ans=int(fi)+int(se)
+		
+	elif op == "2":
+		ans=int(fi)-int(se)
+	elif op == "3":
+		ans=int(fi)*int(se)	
+	elif op=="4":
+		if se=="0":
+			box(None, "Cant divide by Zero(0)","Error!", 0)
+			print("----------------------")	
+			print(" |||CALCULATOR V2.0|||")
+			print("----------------------")	
+			run()
+		else:
+			ans=int(fi)/int(se)
+	print(" |Answer=",ans)	
+	print("******************")		
+		
+	
+def run():
+	start()
+	
+	
+	cal(first,second)
+	again=input("Do you need to calculate again? Y/N:")
+	if again=="y":
+		print("----------------------")	
+		print(" |||CALCULATOR V2.0|||")
+		print("----------------------")	
+		run()	
+		
+	else:
+		sys.exit(0) #exit the program
+	
+	
+print("----------------------")	
+print(" |||CALCULATOR V2.0|||")
+print("----------------------")	
+run()	
+	
